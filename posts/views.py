091fa8589caaaa8ea59d7d7ac44e51c88326ee74
Ahmed Nader <ahmednader42@gmail.com>
2021-07-04 01:08:57 +0200

    Finished authorization and documentation

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,16 @@
 from .models import Post
-from rest_framework import generics
-from .serializers import PostSerializer
+from rest_framework import viewsets
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
-
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
